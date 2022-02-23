894eea7ce76c44f9381fc639de4adddf254c4f95
boysers <boyser2015@gmail.com>
2022-02-23 18:07:43 +0100

    (patch) connect bdd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { ShopController } from './shop/shop.controller';
 import { ShopService } from './shop/shop.service';
-import { ShopModule } from './shop/shop.module';
+// import { ShopModule } from './shop/shop.module';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(`mongodb://localhost/api-nestjs-shop`)
+  .then(() => {
+    console.log('Connexion à MongoDB réussie !');
+  })
+  .catch(() => {
+    console.log('Connexion à MongoDB échouée !');
+  });
 
 @Module({
-  imports: [
+  /* imports: [
     MongooseModule.forRoot('mongodb://localhost/api-nestjs-shop'),
     ShopModule,
-  ],
+  ], */
   controllers: [AppController, ShopController],
   providers: [AppService, ShopService],
 })
